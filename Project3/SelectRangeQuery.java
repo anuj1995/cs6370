0231830db4e459f3d5faef145456079076fe280f
adaveinthelife <33740680+adaveinthelife@users.noreply.github.com>
2018-10-30 13:32:28 -0400

    Fixed resultTest index bugs

--- Project3/SelectRangeQuery.java
+++ Project3/SelectRangeQuery.java
@@ -46,10 +46,10 @@ public class SelectRangeQuery {
         var tables = new String [] { "Student", "Professor", "Course", "Teaching", "Transcript" };
         var tups   = new int [] { 10000, 1000, 2000, 50000, 5000 };
         
-	//Create relations (student, professor, course, teaching, transcript)
-	Table studentRelation = new Table("Student", "id name address status", "Integer String String String", "id");
-	Table professorRelation = new Table ("Professor", "id name deptId", "Integer String String", "id");
-	Table courseRelation = new Table ("Course", "crsCode deptId crsName descr", "String String String String", "crsCode");
+	    //Create relations (student, professor, course, teaching, transcript)
+	    Table studentRelation = new Table("Student", "id name address status", "Integer String String String", "id");
+	    Table professorRelation = new Table ("Professor", "id name deptId",	"Integer String String", "id");
+	    Table courseRelation = new Table ("Course", "crsCode deptId crsName descr", "String String String String", "crsCode");
         Table teachingRelation = new Table ("Teaching", "crsCode semester profId","String String Integer", "crsCode semester");
         Table transcriptRelation = new Table ("Transcript", "studId crsCode semester grade","Integer String String String", "studId crsCode semester");
     
@@ -66,9 +66,26 @@ public class SelectRangeQuery {
             out.println ();
         } // for
         
-      //Insert tuples into student relation
-        for (int i = 0; i < resultTest [0].length; i++) {
+      //Insert tuples into various relations
+        	//STUDENT//
+        for (int i = 0; i < resultTest[0].length; i++) {
             studentRelation.insert(resultTest[0][i]);
+        }
+        	//PROFESSOR//
+        for (int i = 0; i < resultTest[1].length; i++) {
+            professorRelation.insert(resultTest[1][i]);
+        }
+        	//COURSE//
+        for (int i = 0; i < resultTest[2].length; i++) {
+            courseRelation.insert(resultTest[2][i]);
+        }
+        	//TEACHING//
+        for (int i = 0; i < resultTest[3].length; i++) {
+            teachingRelation.insert(resultTest[3][i]);
+        }
+        	//TRANSCRIPT//
+        for (int i = 0; i < resultTest[4].length; i++) {
+            transcriptRelation.insert(resultTest[4][i]);
         }
         
         //Random generator to create index
@@ -77,49 +94,45 @@ public class SelectRangeQuery {
         //Create indexes for each relation
         int studentIndex, professorIndex, courseIndex, teachingIndex, transcriptIndex;
         
+        //Select Range Query with 10000 tuples
+        for( int i = 0; i < 25 ; i++){
+        	studentIndex = randomGenerator.nextInt(10000) + 1;
+        	System.out.print("Student Index = " + studentIndex);
+            KeyType k = new KeyType(studentIndex);
+            studentRelation.select(k);
+        }
+        
         //Select Range Query with 1000 tuples
-        for( int i = 0; i < 100 ; i++){
-		studentIndex = randomGenerator.nextInt(1000) + 1;
-            KeyType k = new KeyType(resultTest[0][studentIndex][0]);
+        for( int i = 0; i < 25 ; i++){
+        	professorIndex = randomGenerator.nextInt(1000) + 1;
+        	System.out.print("Professor Index = " + professorIndex);
+            KeyType k = new KeyType(resultTest[1][professorIndex][0]);
             professorRelation.select(k);
         }
         
-        //Select Range Query with 5000 tuples
-        for( int i = 0; i < 100 ; i++){
-        	professorIndex = randomGenerator.nextInt(5000) + 1;
-            KeyType k = new KeyType(resultTest[0][professorIndex][0]);
-            professorRelation.select(k);
-        }
-        
-        //Select Range Query with 7000 tuples
-        for( int i = 0; i < 100 ; i++){
-        	courseIndex = randomGenerator.nextInt(7000) + 1;
-            KeyType k = new KeyType(resultTest[0][courseIndex][0]);
+        //Select Range Query with 2000 tuples
+        for( int i = 0; i < 25 ; i++){
+        	courseIndex = randomGenerator.nextInt(2000) + 1;
+        	System.out.print("Course Index = " + courseIndex);
+            KeyType k = new KeyType(resultTest[2][courseIndex][0]);
             courseRelation.select(k);
         }
         
-        //Select Range Query with 10000 tuples
-        for( int i = 0; i < 100 ; i++){
-        	teachingIndex = randomGenerator.nextInt(10000) + 1;
-            KeyType k = new KeyType(resultTest[0][teachingIndex][0]);
+        //Select Range Query with 50000 tuples
+        for( int i = 0; i < 25 ; i++){
+        	teachingIndex = randomGenerator.nextInt(50000) + 1;
+        	System.out.print("Teaching Index = " + teachingIndex);
+            KeyType k = new KeyType(resultTest[3][teachingIndex][0]);
             teachingRelation.select(k);
         }
         
-        //Select Range Query with 50000 tuples
-        for( int i = 0; i < 100 ; i++){
-        	transcriptIndex = randomGenerator.nextInt(50000) + 1;
-            KeyType k = new KeyType(resultTest[0][transcriptIndex][0]);
+        //Select Range Query with 5000 tuples
+        for( int i = 0; i < 25 ; i++){
+        	transcriptIndex = randomGenerator.nextInt(5000) + 1;
+        	System.out.print("Transcript Index = " + transcriptIndex);
+            KeyType k = new KeyType(resultTest[4][transcriptIndex][0]);
             transcriptRelation.select(k);
         }
-        
-        //Join Query
-        for( int i = 0; i < 100 ; i++){
-            Table results = studentRelation.join("id", "studId", transcriptRelation);
-            }
-        
-        //Print relations
-        studentRelation.printIndex();
-        transcriptRelation.printIndex();
         
     } // main
 }
